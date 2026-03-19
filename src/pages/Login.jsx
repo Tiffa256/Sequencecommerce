fb317c418317ef04784caeaa4482035ba25f46e2
Tiffa256 <tiffashan10@gmail.com>
2026-03-19 05:56:28 +0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -100,7 +100,7 @@ function SpinnerOverlay({ duration = 500, onDone }) {
   );
 }
 
-const API_URL = "https://sequence-commerce-nsph.onrender.com";
+const API_URL = "https://sequence2-backend.onrender.com";
 
 export default function Login({ refreshRecords }) {
   const [input, setInput] = useState("");
